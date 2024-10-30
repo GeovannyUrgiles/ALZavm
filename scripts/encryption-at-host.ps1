181155ac9918606c2d4420b2a633a94e61410ef7
John Kaufman <john.kaufman@neudesic.com>
2024-10-30 12:48:01 -0500

    vm

---
+++ scripts/encryption-at-host.ps1
@@ -0,0 +1,5 @@
+﻿azure
+
+Register-AzProviderFeature -FeatureName "EncryptionAtHost" -ProviderNamespace "Microsoft.Compute"
+
+New-AzRoleAssignment -ApplicationId "c56384d7-d782-4ecd-8e9c-9a9f79161b41" -Scope "/" -RoleDefinitionName "Owner"
